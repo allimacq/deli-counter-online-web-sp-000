4718d4767232166af7300759de9a6afd3ff90135
Allison MacQueen <allimacq@gmail.com>
2020-08-29 02:24:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -32,6 +32,6 @@ end
 def take_a_number(katz_deli, new_customer)
   position = katz_deli.length() + 1
   p position
-  puts "Welcome, #{new_customer}. You are number #{position} in line."
-  puts "HHHHH"
+  p "Welcome, #{new_customer}. You are number #{position} in line."
+  p "HHHHH"
 end
